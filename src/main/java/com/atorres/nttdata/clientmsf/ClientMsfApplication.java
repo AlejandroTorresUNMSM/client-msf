8d29dfe6f51c52c5bd9c21f72aa06af2be5dbd44
AlejandroTorresUNMSM <alephdmc2575@gmail.com>
2023-07-27 10:59:53 -0500

    dev: commit init

---
+++ src/main/java/com/atorres/nttdata/clientmsf/ClientMsfApplication.java
@@ -0,0 +1,13 @@
+package com.atorres.nttdata.clientmsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientMsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientMsfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atorres/nttdata/clientmsf/ClientMsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atorres.nttdata.clientmsf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientMsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
